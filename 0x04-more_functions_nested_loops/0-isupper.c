74316b40cac4398046a6fc8f76a18d6d6aae9895
asandasulelo <suleloa2@gmail.com>
2023-02-22 23:35:37 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - Check of a letter is supper
+ * @x: The number to be checked or 0 for any else
+ * Return: 1 for upper letter or 0 for any else
+ */
+
+int _isupper(int x)
+{
+	if (x >= 65 && x <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
